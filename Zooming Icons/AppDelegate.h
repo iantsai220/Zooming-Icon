24a67c7d50553e9a5d352448f6bac5413ed08524
iantsai <iantsai86@gmail.com>
2015-06-11 10:56:01 -0700

    Initial Commit

---
+++ Zooming Icons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zooming Icons
+//
+//  Created by Ian Tsai on 2015-06-11.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zooming Icons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zooming Icons
+//
+//  Created by Ian Tsai on 2015-06-11.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
